6a55834c3a5d2387fbfe5c39620bfb89c3e0b30b
raish123 <shaikhraishazam@gmail.com>
2025-12-05 00:56:06 +0530

    working understanding complex workflow of langGraph

---
+++ MyProject/config/__init__.py
@@ -0,0 +1,1 @@
+from config.config_variable import * 
--- MyProject/config/config_variable.py
+++ MyProject/config/config_variable.py
@@ -1,10 +1,13 @@
 from dotenv import load_dotenv
 import os,sys
-
-#loading all these api key to this modules!!
-load_dotenv()
+from logger_config import logger
+from exception import CustomException
 
 
 try:
-    pass
-except 
+    logger.info(msg="Loading the environment Variable")
+    load_dotenv()
+    logger.info(msg="Environment Variable successfully Loaded")
+
+except Exception as e:
+    raise CustomException(e,sys)
