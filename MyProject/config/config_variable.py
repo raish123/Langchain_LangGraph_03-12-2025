df9d4a25464ce88536b4cbf6ef2a9cca4a9588f6
raish123 <shaikhraishazam@gmail.com>
2025-12-13 23:01:23 +0530

    advance rag jupyter notebook jisme pinecone database storing dense or sparse vector and doing hybrid search to get relevant document and doing cohere reranking to get relevant document apply contextualcompression retrievel

--- MyProject/config/config_variable.py
+++ MyProject/config/config_variable.py
@@ -7,6 +7,17 @@ from exception import CustomException
 try:
     logger.info(msg="Loading the environment Variable")
     load_dotenv()
+    AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
+    AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
+    AWS_REGION = os.getenv("AWS_REGION")
+
+    S3_BUCKET_NAME = os.getenv("S3_BUCKET_NAME")
+    S3_PREFIX = os.getenv("S3_PREFIX")
+    
+    PINECONE_API_KEY = os.getenv("PINECONE_API_KEY")
+    INDEX_NAME = os.getenv("INDEX_NAME")
+    COHERE_API_KEY = os.getenv("COHERE_API_KEY")
+
     logger.info(msg="Environment Variable successfully Loaded")
 
 except Exception as e:
